40018c90ca3e2b74eb50dacfcaaf0c24e3280a85
vovanolo <vpankiv@softserveinc.com>
2023-07-12 19:32:51 +0300

    lesson1

---
+++ app.js
@@ -0,0 +1,54 @@
+// console.log("hello"); // вивід в консоль hello
+// коментар
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+
+
+// змінні
+
+// let var const
+
+// let message = "hello everybody";
+
+// console.log(message);
+
+// let name = "Volodymyr";
+// name = "Ivan";
+// const name = "Volodymyr";
+// name = "Ivan";
+
+// let name = "Volodymyr";
+// // name = "Ivan";
+// name = 5;
+
+// console.log("hello " + name);
+// const user = {
+//     name: "Volodymyr",
+//     age: 28,
+//     online: true,
+//     house: null,
+//     wife: unde
+// }
+
+// let age = 2000;
+// let name  = "Volodymyr";
+
+// console.log(typeof age);
+// console.log(typeof(name));
+// let name = "Volodymyr";
+// let age = 2000;
+// let email = "mail@mail";
+
+// console.log("Hello " + name + " your age is " + age + "email: " + email);
+// console.log(`Hello ${name} your age is ${age} email is ${email}`);
+
+// alert("hello from js");
+
+// let name = prompt("hello whats your name?");
+
+// alert("Hello " + name);
+
+// let ok = confirm("Are you okay?");
+// alert("the user is ok? - " + ok);
+
