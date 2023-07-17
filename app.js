bdcc89614be19958f21f420a4bbb571cfccd3256
vovanolo <vpankiv@softserveinc.com>
2023-07-17 19:43:26 +0300

    lesson 2

--- app.js
+++ app.js
@@ -52,3 +52,120 @@
 // let ok = confirm("Are you okay?");
 // alert("the user is ok? - " + ok);
 
+
+// lesson 2
+
+// let i = 1;
+
+// i = i + 1;
+// i++;
+// i+=1;
+// i--
+// i-=1;
+
+// Math
+
+// Math.pow(2,4); - степінь
+
+// Math.sqrt(16);
+
+// console.log(10 + "5"); - "105"
+
+// console.log(5 +"5"+5); - "555"
+
+// console.log(10 >= 10)
+
+// false - 0, ""
+// true = 1, "sf"
+
+// let age = +prompt("how old are you?");
+
+// let result = (age > 18) ? "you are adult" : "you are young";
+
+// console.log(result);
+
+// const students = ["Ivan", "Petro", "Marcel"];
+
+
+// console.log(students);
+// console.log(students[2]);
+
+// students[0] = "John";
+
+// console.log(students);
+
+// students[3] = "John";
+
+// students[6] = "Max";
+// console.log(students);
+
+
+// console.log(students.length);
+
+
+// const students = ["Ivan", "Petro", "Marcel"];
+
+
+// students.push("John");
+// students.pop();
+
+// console.log(students);
+// let age = +prompt("how old are you?");
+
+// if(age > 18){
+//     console.log("you are adult");
+// }
+
+// if(age > 18){
+//     console.log("you are adult");
+// }else{
+//     console.log("you are too young");
+// }
+
+// if(age > 18){
+//     console.log("you are adult");
+// }else if(age < 18){
+//     console.log("you are too young");
+// }else{
+//     console.log("wrong age");
+// }
+
+// if(age > 18 && age < 25){
+//     console.log("age is between 18 and 25");
+// }else if(age > 25 && age < 35){
+//     console.log("age is between 25 and 35");
+// }else if (age > 35 && 45){
+//     console.log("age is between 35 and 45");
+// }
+
+// let age = +prompt("how old are you?");
+
+// switch(true){
+//     case age > 18 && age < 25:
+//         onsole.log("age is between 18 and 25");
+//         break;
+//     case age > 25 && age < 35:
+//         console.log("age is between 25 and 35"); 
+//         break;   
+//     case age > 35 && age < 45:
+//         console.log("age is between 35 and 45");
+//         break;  
+//     default:
+//         console.log("you are not 18 and not 25");
+// }
+
+// switch(age){
+//     case 18:
+//         console.log("you are 18");
+//         break;
+//     case 25:
+//         console.log("you are 25"); 
+//         break;   
+//     default:
+//         console.log("you are not 18 and not 25");
+
+
+// }
+
+
+
